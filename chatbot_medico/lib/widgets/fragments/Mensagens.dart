727e3d95fa27194a99bf3fafd599719dae9211eb
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2019-06-18 19:11:23 -0300

    ATT_18/06/2019_2
    Adicionado
    
    -Modelo de balões para mensagens
    -ListView para mensagens

--- chatbot_medico/lib/widgets/fragments/Mensagens.dart
+++ chatbot_medico/lib/widgets/fragments/Mensagens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Mensagens extends StatelessWidget {
   @override
@@ -23,65 +24,109 @@ class _TelaDeMensagens extends State<TelaMensagens> {
 
   @override
   Widget build(BuildContext context) {
-    
+    List<Widget> conversa = gerarConversa();
+
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text(usuario),
         ),
         body: Container(
-          padding: EdgeInsets.only(left: 6,right: 4,bottom: 8),
+          color: Colors.white,
+          padding: EdgeInsets.only(left: 6, right: 4, bottom: 8),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[enviaMsg(),recebeMesg(),
-              //envio de mensagem
+            children: <Widget>[
+              //conversa[0],
+              //conversa[1],
+              //conversa[3],
+              //conversa[4],
+              //conversa[4],
               Container(
-                  child: Row(children: <Widget>[
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "mensagem",
-                      hintStyle: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.2)),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(80.0))),
+                  height: 500,
+                  child: ListView.builder(
+                        
+                      itemBuilder: (context, pos) {
+                        return Card(
+                            elevation: 0,
+                            color: Colors.white,
+                            child: conversa[pos]);
+                      },
+                      itemCount: conversa.length)),
+              Container(
+                  child: Row(
+                children: <Widget>[
+                  Flexible(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "mensagem",
+                          hintStyle:
+                              TextStyle(color: Color.fromRGBO(0, 0, 0, 0.2)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(80.0))),
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.only(left: 5),child:FloatingActionButton(child: Icon(Icons.send),) ,)
-              ],)),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.send),
+                    ),
+                  )
+                ],
+              )),
             ],
           ),
         ));
   }
 
-  Widget recebeMesg(){
-
+  // balão de recebe
+  Widget recebeMesg() {
     return Container(
+      constraints: BoxConstraints(maxWidth: 270),
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.only(bottom: 12),
-      width: 270,
-      decoration: BoxDecoration(color:Color.fromRGBO(0, 0, 255, 0.3),borderRadius: BorderRadius.circular(30.0)),
-      child: Text("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(120, 120, 120, 0.5),
+          borderRadius: BorderRadius.circular(30.0)),
+      child: Text(
+          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
     );
   }
-  
-  Widget enviaMsg(){
 
+  // balão de envio mensagem
+  Widget enviaMsg() {
     return Container(
+      constraints: BoxConstraints(maxWidth: 270),
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.only(bottom: 12),
-      width: 270,
-      decoration: BoxDecoration(color:Color.fromRGBO(120, 120, 120, 0.5),borderRadius: BorderRadius.circular(30.0)),
-      child: Text("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(0, 0, 255, 0.3),
+          borderRadius: BorderRadius.circular(30.0)),
+      child: Text("BBBBBBBBBBBBBBBBBBBBBBBBBBB"),
     );
+  }
+
+  List<Widget> gerarConversa() {
+    List<Widget> exp = List<Widget>();
+
+    for (int i = 0; i < 30; i++) {
+      exp.add(Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Flexible(
+            child: enviaMsg(),
+          )
+        ],
+      ));
+      exp.add(Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Flexible(
+            child: recebeMesg(),
+          )
+        ],
+      ));
+    }
+    return exp;
   }
 }
-
-/*
- ListView.builder(
-              itemBuilder: (context, pos) {
-                return Card(
-                    child: Text(androidVenNames[pos],
-                        style: TextStyle(fontSize: 22)));
-              },
-              itemCount: androidVenNames.length)
-  */
